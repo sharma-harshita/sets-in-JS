ff3cdd042c647720bf228fe6da1084d6ed3a6857
harshita.sharma <harshita.sharma@asksid.ai>
2022-01-04 20:36:29 +0530

    changes

--- test.js
+++ test.js
@@ -34,6 +34,8 @@ console.log(newNum);
 
 // Basic functions which set provides.
 
+// add, has, size is a property, has, clear, delete
+
 
 // add funtion 
 let mySet = new Set();
@@ -45,15 +47,27 @@ mySet.add([1,2,3,4,5])   // array value
 mySet.add(true)   // boolean value
 mySet.add({id:1, name:"john"})   // obj value
 
+
 console.log(mySet);
 
 
-// has function 
+// has function == checks if the element exists or not
 // returns you boolean value
 
-console.log("has function",mySet.has(12));
-console.log("has function2",mySet.has("elevation"));
+console.log("has function", mySet.has(12));
+console.log("has function2", mySet.has("elevation"));
+console.log("has function2", mySet.has(true));
 
 
 // size == how many elements are present in the set
 console.log(mySet.size);
+
+
+// delete
+mySet.delete(12)
+console.log(mySet);
+
+
+// clear == deletes all the elements from the set 
+mySet.clear();
+console.log(mySet);
